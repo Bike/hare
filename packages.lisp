b39008cdb81a4b86c071dc3088232659d15e3925
Bike <aeshtaer@gmail.com>
2020-03-28 18:55:58 -0400

    Export more stuff

--- packages.lisp
+++ packages.lisp
@@ -1,3 +1,19 @@
 (defpackage #:hare
   (:use #:cl)
-  (:shadow #:type #:case #:variable))
+  (:shadow #:type #:case #:variable)
+  (:export #:with-type-cache
+           #:int #:int-type-length
+           #:pointer #:pointer-type-underlying
+           #:fun #:fun-return #:parameters
+           #:arrayt :arrayt-element-type
+           #:adt-def #:name #:tvars #:constructors #:members
+           #:adt #:adt-args
+           #:subst-type)
+  (:export #:ast
+           #:seq #:asts
+           #:call #:callee #:args
+           #:literal #:initializer
+           #:reference #:variable
+           #:branch #:test #:then #:else
+           #:bind #:value #:body)
+  (:export #:unify))
