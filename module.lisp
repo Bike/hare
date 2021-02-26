09b98408f3c87185ef079e40a5cfa49a4e555e2d
Bike <aeshtaer@gmail.com>
2021-02-26 12:57:56 -0500

    Fix MODULE to actually work

--- module.lisp
+++ module.lisp
@@ -16,16 +16,20 @@
 
 (defun module (pre-module)
   (assert (null (toplevels pre-module)))
-  (let* ((varbinds (varbinds pre-module))
-         (tmap
-           (loop for (variable) in varbinds
-                 ;; FIXME: We're straight up ignoring type declamations here.
-                 for tv = (make-tvar (name variable))
-                 for sc = (schema tv (list tv))
-                 collect (cons variable sc)))
-         (tenv (make-tenv tmap))
-         (entries
-           (loop for (variable . initializer) in varbinds
-                 for inference = (infer-initializer-toplevel initializer
-                                                             tenv))))
-    (make-instance 'module :entries entries)))
+  (with-type-cache ()
+    (let* ((varbinds (varbinds pre-module))
+           (tmap
+             (loop for (variable) in varbinds
+                   ;; FIXME: We're straight up ignoring type declamations here.
+                   for tv = (make-tvar (name variable))
+                   for sc = (schema tv (list tv))
+                   collect (cons variable sc)))
+           (tenv (make-tenv tmap))
+           (entries
+             (loop for (variable . initializer) in varbinds
+                   for inference = (infer-initializer-toplevel initializer
+                                                               tenv)
+                   collect (make-instance 'entry
+                             :variable variable :initializer initializer
+                             :inference inference))))
+      (make-instance 'module :entries entries))))
